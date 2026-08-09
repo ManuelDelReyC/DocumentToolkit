1bfdbdd0c333546ce866f89762160d8de4cd3cda
Manuel <manuel.delrey@yahoo.com>
2026-08-09 17:28:01 +0200

    Add OCR support and movement classification

--- src/documenttoolkit/classifier.py
+++ src/documenttoolkit/classifier.py
@@ -4,4 +4,6 @@ def classify_document(texto):
         return "HIPOTECA"
     elif "EXTRACTO" in texto_norm or "OPERACIONES" in texto_norm:
         return "EXTRACTO_BANCARIO"
+    elif "DETALLE DEL MOVIMIENTO" in texto_norm:
+        return "MOVIMIENTO_BANCARIO"
     return "OTRO"
--- src/documenttoolkit/ocr_reader.py
+++ src/documenttoolkit/ocr_reader.py
@@ -3,16 +3,16 @@ import pytesseract
 
 
 def extract_text_ocr(pdf_path):
-    paginas = convert_from_path(pdf_path)
+    paginas = convert_from_path(pdf_path)   # Converts the pdf file into several images, one per page
 
     texto = ""
 
-    for pagina in paginas:
-        contenido = pytesseract.image_to_string(
+    for pagina in paginas:   # Loop over each page
+        contenido = pytesseract.image_to_string(        # OCR page by page
             pagina,
             lang="spa"
         )
 
-        texto += contenido
+        texto += contenido      # Cumululate text of all pages into a single string
 
     return texto.strip()
